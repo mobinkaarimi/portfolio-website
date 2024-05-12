8b43d23c7793c6717eafde0112e6ff41a8dc1aa9
mobinkaarimi <68745874+mobinkaarimi@users.noreply.github.com>
2024-05-13 00:32:55 +0330

    update[vite.config.ts]-> ignore ts warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// @ts-ignore
 import { resolve } from "path";
 
+// @ts-ignore
 const root = resolve(__dirname, "src");
 export default defineConfig({
   plugins: [react()],
