96b89181cbd37e48f69ec36e0d51fc5767643b60
mobinkaarimi <68745874+mobinkaarimi@users.noreply.github.com>
2024-05-13 00:28:20 +0330

    update[tailwind.config.js]-> add color & font palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      "palette-bold": ["Barlow-Condensed-Extra-Bold-Italic", "sans-serif"],
+      "palette-regular": ["Barlow-Condensed-Extra-Bold-Italic", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        palette: {
+          "primary-yellow": "#ce8622",
+          "primary-gray": "#121212",
+        },
+      },
+    },
   },
   plugins: [],
 };
